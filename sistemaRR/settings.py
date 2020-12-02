4143653aca17e283c41696829a86a3b629ba7c6f
Rafael Araujo <rafael.araujo@prosaude.org.br>
2020-12-02 10:26:28 -0300

    CORS

--- sistemaRR/settings.py
+++ sistemaRR/settings.py
@@ -67,8 +67,8 @@ MIDDLEWARE = [
     
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
     'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'corsheaders.middleware.CorsPostCsrfMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
