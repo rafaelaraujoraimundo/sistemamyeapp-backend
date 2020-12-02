8412cda8c9fda4c9c59b3a7e010920a385b4409c
Rafael Araujo <rafael.araujo@prosaude.org.br>
2020-12-02 07:45:42 -0300

    Deploy da Aplicacao

--- sistemaRR/settings.py
+++ sistemaRR/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '0s3^)s3xotyvj19nv^)h2dvcs4r+k@*!!&9gd*8z7(d-%n06)%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['backendmyeapp.herokuapp.com']
 
 # Application definition
 
