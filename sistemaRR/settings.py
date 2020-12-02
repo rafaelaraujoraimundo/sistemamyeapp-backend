8473cb1df0c1204f12aaefb184fbcb53a352adc5
Rafael Araujo <rafael.araujo@prosaude.org.br>
2020-12-02 07:41:53 -0300

    Deploy da Aplicacao

--- sistemaRR/settings.py
+++ sistemaRR/settings.py
@@ -156,10 +156,6 @@ AUTH_USER_MODEL = 'core.CustomUsuario'
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / "staticfiles",
-]
-
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 MEDIA_URL = '/media/'
