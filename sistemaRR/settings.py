3b80b7706e957c9a2224b5e0d6a2ee90d93c04fd
Rafael Araujo <rafael.araujo@prosaude.org.br>
2020-12-02 09:24:52 -0300

    json data

--- sistemaRR/settings.py
+++ sistemaRR/settings.py
@@ -99,12 +99,12 @@ WSGI_APPLICATION = 'sistemaRR.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-""" DATABASES = {
+"""DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-} """
+}"""
 
 DATABASES = {
     "default": {
