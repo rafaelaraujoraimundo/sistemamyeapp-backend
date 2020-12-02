203a2d46605ffc20542df8ba4150708e90b496a9
Rafael Araujo <rafael.araujo@prosaude.org.br>
2020-12-02 09:53:37 -0300

    json data

--- sistemaRR/settings.py
+++ sistemaRR/settings.py
@@ -209,6 +209,14 @@ REST_AUTH_SERIALIZERS = {
 
 CORS_ORIGIN_ALLOW_ALL = True
 
+CORS_ALLOW_HEADERS = (
+        'x-requested-with',
+        'content-type',
+        'accept',
+        'origin',
+        'authorization',
+        'x-csrftoken'
+)
 
 
 # Habilitar Heroku
